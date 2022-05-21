7b3cb7f86fa39897f723511aafd8b01c54d9094f
Harshal0902 <harshalraikwar07@gmail.com>
2022-05-22 00:08:11 +0530

    ✨ Add server

--- client/src/container/Main.js
+++ client/src/container/Main.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from '../components/Navbar';
 import Home from '../pages/Home';
 import Chatbot from '../components/Chatbot';
+import Chatroom from '../pages/Chatroom';
 import Footer from '../components/Footer';
 
 function Main() {
@@ -10,6 +11,7 @@ function Main() {
             <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/chatroom" element={<Chatroom />} />
             </Routes>
             <Chatbot />
             <Footer />
